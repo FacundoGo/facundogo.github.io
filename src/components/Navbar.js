75f8df6274a2db640e3f10d144c3869f4af9c781
Facundo Gomez <gomezvera.f@gmail.com>
2021-03-17 18:44:28 +0100

    make nav links through react router dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react'
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom'
 
 export default class Navbar extends Component {
   render() {
     return (
       <Nav className="justify-content-center" activeKey="/home">
             <Nav.Item>
-        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link><Link to="/">Home</Link></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/Experience">Experience</Nav.Link>
+        <Nav.Link><Link to="/Experience">Experience</Link></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/Education">Education</Nav.Link>
+        <Nav.Link><Link to="/Education">Education</Link></Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/Contact">Contact</Nav.Link>
+        <Nav.Link><Link to="/Contact">Contact</Link></Nav.Link>
       </Nav.Item>
     </Nav>
     )
